24c1d429f021e541b6abac653d4751553f2acaf1
Aubin Olivrie <aubin.olivrie@qcmfrance.com>
2023-03-03 19:20:01 +0100

    force role owner createLocation

--- src/routes/location.route.js
+++ src/routes/location.route.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const LocationController = require('../controllers/location.controller');
 const verifyToken = require('../middlewares/verifyToken');
+const verifyOwner = require('../middlewares/verifyOwner');
 
 router.get('/', LocationController.getLocations);
 router.get('/user', verifyToken, LocationController.getMyLocations);
 router.get('/:id', LocationController.getLocationById);
-router.post('/', verifyToken, LocationController.createLocation);
+router.post('/', verifyToken, verifyOwner, LocationController.createLocation);
 router.put('/', verifyToken, LocationController.updateLocation);
 
 module.exports = router;
